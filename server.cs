8b4b6673a97a8eca6fa61117163c36621bbe0f3a
Simon Boorer <Simon Boorer>
2017-03-21 00:30:56 +1100

    Initial commit of Vehicle Base and Minifig Seat

---
+++ server.cs
@@ -0,0 +1,17 @@
+datablock fxDTSBrickData(brick4x10VehicleBaseData)
+{
+	brickFile = "./4x10vehicleBase.blb";
+	category = "Special";
+	subCategory = "Misc";
+	uiName = "4x10 Vehicle Base";
+	iconName = "Add-Ons/Brick_Vehicles/4x10 Vehicle Base";
+};
+
+datablock fxDTSBrickData(brick2x2minifigSeatData)
+{
+	brickFile = "./2x2minifigSeat.blb";
+	category = "Special";
+	subCategory = "Misc";
+	uiName = "2x2 Minifig Seat";
+	iconName = "Add-Ons/Brick_Vehicles/2x2 Minifig Seat";
+};
